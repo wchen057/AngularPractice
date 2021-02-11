11f65ba13d94bdbd9386355e20441dbaedc2fe00
ritch.chan <ritchchan@hotmail.com>
2021-02-11 10:25:05 -0500

    remove hero-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
