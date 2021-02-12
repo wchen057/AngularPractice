2bbc8d11f2078280a6099c47b3123ecf5b247fa5
ritch.chan <ritchchan@hotmail.com>
2021-02-12 08:25:06 -0500

    successfully running

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { MessagesComponent } from './messages/messages.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
+    MessagesComponent,
+    HeroDetailComponent,
   ],
   imports: [
     BrowserModule,
