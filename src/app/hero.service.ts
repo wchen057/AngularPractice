ffea069569b502f39135633373a5cf4d97449282
ritch.chan <ritchchan@hotmail.com>
2021-02-13 15:40:17 -0500

    add app-routing modules

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,16 +6,18 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero | undefined> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
